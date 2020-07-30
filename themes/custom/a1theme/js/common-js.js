5e3afb77511ca31f0d12354f16b4a6bb7a5f1acd
nikhil <nikhildongre6@gmail.com>
2020-07-30 18:22:23 +0530

    first commit

---
+++ themes/custom/a1theme/js/common-js.js
@@ -0,0 +1,57 @@
+/*window.onload = function() {
+  if (window.jQuery) {
+    // jQuery is loaded
+    alert("Yeah!");
+  } else {
+    // jQuery is not loaded
+    alert("Doesn't Work");
+  }
+} */
+
+//alert("jjojoj");
+// $(document).ready(function(){
+ 
+//   alert("jjojoj");
+// });
+	
+// jQuery(document).ready(function(){ 
+//   console.log("hello");
+  
+//   ("#slick-views-blog-block-1-1").slick({
+//   dots: true,
+//   arrows: false,
+//   centerMode: true 
+// });
+
+// });
+
+ // $('.your-class').slick({
+  //   setting-name: setting-value
+  // });
+
+
+
+
+// $('.autoplay').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+// slick-views-blog-block-1-1-slider 
+//slick-views-blog-block-1-1
+
+// jQuery(document).ready(function(){
+//      console.log("hello");
+// // window.onload=function(){
+//   $slideshow = $('#blog_slider').slick({
+//     dots:true,
+//   autoplay:true,
+//   arrows:false
+
+//   });
+// });
+
+// $('button.next').click(function(){
+//   $("#slick-views-blog-block-1-1-slider").slickPrev();
+// });
